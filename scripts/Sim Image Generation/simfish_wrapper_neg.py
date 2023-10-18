3ed5bb77545f9dee6e4710c994dc8f59521eb41d
Blythe <35576053+bhospelhorn@users.noreply.github.com>
2023-10-18 17:41:58 -0500

    simneg script and additional stats table

---
+++ scripts/Sim Image Generation/simfish_wrapper_neg.py
@@ -0,0 +1,65 @@
+import os
+import random
+import simfish
+import bigfish.stack
+
+def saveSimImage(sim_results, outdir, nameroot):
+    bigfish.stack.save_data_to_csv(sim_results[1], os.path.join(outdir, nameroot + "_key.csv"), delimiter=',')
+
+    simimg = sim_results[0]
+    Z = simimg.shape[0]
+
+    for z in range(Z):
+        outsuff = nameroot + f'_imgz_{z:04d}.csv'
+        bigfish.stack.save_data_to_csv(simimg[z,:,:], os.path.join(outdir, outsuff), delimiter=',')
+    
+def main(args):
+    outdir = "D:\\usr\\bghos\\labdat\\imgproc\\img\\simneg"
+    
+    dim_xy = 512
+    dim_z = 16
+    
+    voxel_xy = 65
+    voxel_z = 200
+    
+    ptsig_xy = 95
+    ptsig_z = 210
+    
+    bg_level_min = 100
+    bg_level_max = 1500
+    
+    cluster_count = 5
+    spots_per_clust = 3
+    random_sig = 0.15
+    
+    bg_var_min = 0.05
+    bg_var_max = 0.5
+    
+    amplvar_min = 0.05
+    amplvar_max = 0.5
+    
+    amp_min = 0
+    amp_max = 0 * 10
+    
+    spots_min = 12
+    spots_max = 5000
+    
+    genamt = 500
+    
+    outfile = open(os.path.join(outdir, "siminfo.csv"), "w")
+    outfile.write("#ID,BGLevel,BGVar\n")
+    for i in range(genamt):
+        randomid = random.randrange(0, (2 << 31), 1)
+        idstr = f'{randomid:08x}'
+        #spot_amt = random.randrange(spots_min, spots_max, 1)
+        #ampl = random.randrange(amp_min, amp_max, 1)
+        #amplvar = random.uniform(amplvar_min, amplvar_max)
+        bglvl = random.uniform(bg_level_min, bg_level_max)
+        bgvar = random.uniform(bg_var_min, bg_var_max)
+        res = simfish.simulate_image(3, 0, random_n_spots=False, n_clusters=cluster_count, random_n_clusters=True, n_spots_cluster=spots_per_clust, random_n_spots_cluster=True, image_shape=(dim_z, dim_xy, dim_xy), subpixel_factors=None, voxel_size=(voxel_z, voxel_xy, voxel_xy), sigma=(ptsig_z, ptsig_xy, ptsig_xy), random_sigma=random_sig, amplitude=0, random_amplitude=0, noise_level=bglvl, random_noise=bgvar)
+        saveSimImage(res, outdir, idstr)
+        outfile.write(idstr + "," + str(bglvl) + "," + str(bgvar) + "\n")
+    outfile.close()
+    print("hold")
+    
+main(None)
