f1616a55266a46b634ae6a0cecba9c9bfaf411e3
Blythe <35576053+bhospelhorn@users.noreply.github.com>
2024-05-24 20:05:47 -0500

    Figures and chained wrapper script update

--- scripts/MATLAB/test/wrapper_rundir_pipeline_240513.m
+++ scripts/MATLAB/test/wrapper_rundir_pipeline_240513.m
@@ -4,22 +4,20 @@
 TRUESPOT_DIR = '/nobackup/p_neuert_lab/hospelb/TrueSpot_Release';
 SCRIPT_WD = '/nobackup/p_neuert_lab/hospelb/imgproc/slurm/script';
 
-%SCRIPT_DIR_LOCAL = 'C:\Users\bghos\Desktop\slurm';
-SCRIPT_DIR_LOCAL = 'C:\Users\hospelb.VUDS\Desktop\slurm';
+SCRIPT_DIR_LOCAL = 'C:\Users\bghos\Desktop\slurm';
+%SCRIPT_DIR_LOCAL = 'C:\Users\hospelb.VUDS\Desktop\slurm';
 MATLAB_MODULE_NAME = 'MATLAB/2022a';
 
 % ========================== I/O Info ==========================
 
-IN_DIR = '/nobackup/p_neuert_lab/hospelb/RNAFISH/Images/JA20240510';
+IN_DIR = '/nobackup/p_neuert_lab/hospelb/RNAFISH/Images/JA20240514';
 %RECURSIVE = true;
 
-OUT_DIR = '/nobackup/p_neuert_lab/hospelb/RNAFISH/Analysis/JA20240510';
+OUT_DIR = '/nobackup/p_neuert_lab/hospelb/RNAFISH/Analysis/JA20240514';
 
 % ========================== Image Options ==========================
 
-CH_SAMPLE = 3;
-%CH_SAMPLE = 4;
-%CH_SAMPLE = 5;
+CH_SAMPLE = [3 4 5];
 
 CH_LIGHT = 1;
 CH_DAPI = 2;
@@ -33,20 +31,21 @@ CELLSEG_ZMAX = 23;
 GAUSS_RAD = 7;
 TH_PRESET = 0; %Offset from default
 
-TARGET_NAME = 'CTT1';
-PROBE_NAME = 'CY5';
-%TARGET_NAME = 'STL1';
-%PROBE_NAME = 'TMR';
-%TARGET_NAME = 'GPP1';
-%PROBE_NAME = 'AF594';
+%TARGET_NAMES = {'CTT1' 'STL1' 'GPP1'};
+TARGET_NAMES = {'GPD1' 'GPP2' 'HSP12'};
+PROBE_NAMES = {'CY5' 'TMR' 'AF594'};
+TARGET_TYPES = {'mRNA' 'mRNA' 'mRNA'};
 
-TARGET_TYPE = 'mRNA';
 SPECIES_NAME = 'Saccharomyces cerevisiae';
 CELL_TYPE = 'GNY0117 Haploid';
 
 % ========================== Slurm Options ==========================
 
-CPUS_PER_JOB = 4;
+CPUS_PER_JOB_CELLSEG = 2;
+RAM_GB_PER_JOB_CELLSEG = 32;
+HR_PER_JOB_CELLSEG = 4;
+
+CPUS_PER_JOB = 1;
 RAM_GB_PER_JOB = 32;
 HR_PER_JOB = 10;
 
@@ -58,6 +57,8 @@ HR_PER_JOB = 10;
 MASTER_SCRIPT_PATH = [SCRIPT_DIR_LOCAL filesep 'ts_batch_gen.sh'];
 IMAGE_LIST_PATH = [SCRIPT_WD '/imglist.txt'];
 SUBMISSION_SCRIPT_PATH = [SCRIPT_WD '/ts_batch.sh'];
+
+sampleChannelCount = size(CH_SAMPLE, 2);
 
 masterScript = fopen(MASTER_SCRIPT_PATH, 'w');
 
@@ -82,105 +83,144 @@ fprintf(masterScript, '\tfname=${fname%%.*}\n');
 fprintf(masterScript, '\tfname=${fname// /_}\n');
 fprintf(masterScript, '\tfname=${fname//.ome/}\n');
 fprintf(masterScript, '\tfname=${fname//./}\n');
-fprintf(masterScript, '\tiname=${fname}_CH%d\n', CH_SAMPLE);
-fprintf(masterScript, '\techo -e "\\tImage name: ${iname}"\n\n');
+%fprintf(masterScript, '\tiname=${fname}_CH%d\n', CH_SAMPLE);
+fprintf(masterScript, '\techo -e "\\tImage name: ${fname}"\n\n');
 
-fprintf(masterScript, '\tioutDir="${outputDir}/${fname}/CH%d"\n', CH_SAMPLE);
+%fprintf(masterScript, '\tioutDir="${outputDir}/${fname}/CH%d"\n', CH_SAMPLE);
+fprintf(masterScript, '\tioutDir="${outputDir}/${fname}"\n');
 fprintf(masterScript, '\tif [ ! -d "${ioutDir}" ]; then\n');
 fprintf(masterScript, '\t\tmkdir -p "${ioutDir}"\n');
 fprintf(masterScript, '\tfi\n\n');
 
-fprintf(masterScript, '\tmyImgScript="${ioutDir}/${iname}_truespot.sh"\n');
-fprintf(masterScript, '\techo -e "#!/bin/bash\\n" > "${myImgScript}"\n');
-fprintf(masterScript, '\techo -e "module load %s" >> "${myImgScript}"\n\n', MATLAB_MODULE_NAME);
+% fprintf(masterScript, '\tmyImgScript="${ioutDir}/${iname}_truespot.sh"\n');
+% fprintf(masterScript, '\techo -e "#!/bin/bash\\n" > "${myImgScript}"\n');
+% fprintf(masterScript, '\techo -e "module load %s" >> "${myImgScript}"\n\n', MATLAB_MODULE_NAME);
+
+fprintf(masterScript, '\tmyCellsegScript="${ioutDir}/${fname}_truespot_cs.sh"\n');
+fprintf(masterScript, '\techo -e "#!/bin/bash\\n" > "${myCellsegScript}"\n');
+fprintf(masterScript, '\techo -e "module load %s" >> "${myCellsegScript}"\n\n', MATLAB_MODULE_NAME);
 
 %Cellseg
-fprintf(masterScript, '\tcsResFile="${ioutDir}/CellSeg_${iname}.mat"\n');
-fprintf(masterScript, '\techo -e "if [ ! -s \\"${csResFile}\\" ]; then" >> "${myImgScript}"\n');
+fprintf(masterScript, '\tcsResFile="${ioutDir}/CellSeg_${fname}.mat"\n');
+fprintf(masterScript, '\techo -e "if [ ! -s \\"${csResFile}\\" ]; then" >> "${myCellsegScript}"\n');
 fprintf(masterScript, '\techo -e "');
 fprintf(masterScript, '\\t\\"${trueSpotDir}/TrueSpot_CellSeg.sh\\"');
 fprintf(masterScript, ' -input \\"${ipath}\\"');
 fprintf(masterScript, ' -outpath \\"${ioutDir}\\"');
-fprintf(masterScript, ' -imgname \\"${iname}\\"');
+fprintf(masterScript, ' -imgname \\"${fname}\\"');
 fprintf(masterScript, ' -chtotal %d', CH_TOTAL);
 fprintf(masterScript, ' -chlight %d', CH_LIGHT);
 fprintf(masterScript, ' -chnuc %d', CH_DAPI);
 fprintf(masterScript, ' -fzmin %d', CELLSEG_ZMIN);
 fprintf(masterScript, ' -fzmax %d', CELLSEG_ZMAX);
+%fprintf(masterScript, ' -ocellmask \"${ioutDir}/${fname}_cellmask.tif\"');
+%fprintf(masterScript, ' -onucmask \"${ioutDir}/${fname}_nucmask.tif\"');
 fprintf(masterScript, ' -template \\"%s\\"', CELLSEG_PRESET);
-fprintf(masterScript, ' -log \\"${ioutDir}/${iname}_cellseg_mat.log\\"');
-fprintf(masterScript, '" >> "${myImgScript}"\n\n');
-fprintf(masterScript, '\techo -e "else" >> "${myImgScript}"\n');
-fprintf(masterScript, '\techo -e "\\techo -e \\"Cellseg results found! Skipping cell segmentation...\\"" >> "${myImgScript}"\n');
-fprintf(masterScript, '\techo -e "fi\\n" >> "${myImgScript}"\n\n');
+fprintf(masterScript, ' -log \\"${ioutDir}/${fname}_cellseg_mat.log\\"');
+fprintf(masterScript, '" >> "${myCellsegScript}"\n\n');
+fprintf(masterScript, '\techo -e "else" >> "${myCellsegScript}"\n');
+fprintf(masterScript, '\techo -e "\\techo -e \\"Cellseg results found! Skipping cell segmentation...\\"" >> "${myCellsegScript}"\n');
+fprintf(masterScript, '\techo -e "fi\\n" >> "${myCellsegScript}"\n\n');
 
-%Spots
-fprintf(masterScript, '\tspotsResStem="${ioutDir}/${iname}_spotCall"\n');
-fprintf(masterScript, '\techo -e "if [ -s \\"${csResFile}\\" ]; then" >> "${myImgScript}"\n');
-fprintf(masterScript, '\techo -e "');
-fprintf(masterScript, '\\t\\"${trueSpotDir}/TrueSpot_RNASpots.sh\\"');
-fprintf(masterScript, ' -input \\"${ipath}\\"');
-fprintf(masterScript, ' -outstem \\"${spotsResStem}\\"');
-fprintf(masterScript, ' -imgname \\"${iname}\\"');
-fprintf(masterScript, ' -chtotal %d', CH_TOTAL);
-fprintf(masterScript, ' -chlight %d', CH_LIGHT);
-fprintf(masterScript, ' -chsamp %d', CH_SAMPLE);
-fprintf(masterScript, ' -cellseg \\"${csResFile}\\"');
-fprintf(masterScript, ' -voxelsize \\"(%d,%d,%d)\\"', VOX_SIZE(1), VOX_SIZE(2), VOX_SIZE(3));
-fprintf(masterScript, ' -probetype \\"%s\\"', PROBE_NAME);
-fprintf(masterScript, ' -target \\"%s\\"', TARGET_NAME);
-fprintf(masterScript, ' -targettype \\"%s\\"', TARGET_TYPE);
-fprintf(masterScript, ' -species \\"%s\\"', SPECIES_NAME);
-fprintf(masterScript, ' -celltype \\"%s\\"', CELL_TYPE);
-fprintf(masterScript, ' -gaussrad %d', GAUSS_RAD);
-fprintf(masterScript, ' -autominth -automaxth');
-fprintf(masterScript, ' -threads %d', CPUS_PER_JOB);
-if TH_PRESET < 0
-    fprintf(masterScript, ' -precision %d', (TH_PRESET * -1));
-else
-    fprintf(masterScript, ' -sensitivity %d', TH_PRESET);
+%Individual channels...
+for ci = 1:sampleChannelCount
+    sampleCh = CH_SAMPLE(ci);
+
+    fprintf(masterScript, '\n\tiname=${fname}_CH%d\n', sampleCh);
+    fprintf(masterScript, '\tcoutDir=${ioutDir}/CH%d\n', sampleCh);
+    fprintf(masterScript, '\tif [ ! -d "${coutDir}" ]; then\n');
+    fprintf(masterScript, '\t\tmkdir -p "${coutDir}"\n');
+    fprintf(masterScript, '\tfi\n\n');
+
+    fprintf(masterScript, '\tmyChannelScript="${coutDir}/${iname}_truespot.sh"\n');
+    fprintf(masterScript, '\techo -e "#!/bin/bash\\n" > "${myChannelScript}"\n');
+    fprintf(masterScript, '\techo -e "module load %s" >> "${myChannelScript}"\n\n', MATLAB_MODULE_NAME);
+
+    %Spots
+    fprintf(masterScript, '\tspotsResStem="${coutDir}/${iname}_spotCall"\n');
+    fprintf(masterScript, '\techo -e "if [ -s \\"${csResFile}\\" ]; then" >> "${myChannelScript}"\n');
+    fprintf(masterScript, '\techo -e "');
+    fprintf(masterScript, '\\t\\"${trueSpotDir}/TrueSpot_RNASpots.sh\\"');
+    fprintf(masterScript, ' -input \\"${ipath}\\"');
+    fprintf(masterScript, ' -outstem \\"${spotsResStem}\\"');
+    fprintf(masterScript, ' -imgname \\"${iname}\\"');
+    fprintf(masterScript, ' -chtotal %d', CH_TOTAL);
+    fprintf(masterScript, ' -chlight %d', CH_LIGHT);
+    fprintf(masterScript, ' -chsamp %d', sampleCh);
+    fprintf(masterScript, ' -cellseg \\"${csResFile}\\"');
+    fprintf(masterScript, ' -voxelsize \\"(%d,%d,%d)\\"', VOX_SIZE(1), VOX_SIZE(2), VOX_SIZE(3));
+    fprintf(masterScript, ' -probetype \\"%s\\"', PROBE_NAMES{ci});
+    fprintf(masterScript, ' -target \\"%s\\"', TARGET_NAMES{ci});
+    fprintf(masterScript, ' -targettype \\"%s\\"', TARGET_TYPES{ci});
+    fprintf(masterScript, ' -species \\"%s\\"', SPECIES_NAME);
+    fprintf(masterScript, ' -celltype \\"%s\\"', CELL_TYPE);
+    fprintf(masterScript, ' -gaussrad %d', GAUSS_RAD);
+    fprintf(masterScript, ' -autominth -automaxth');
+    fprintf(masterScript, ' -threads %d', CPUS_PER_JOB);
+    if TH_PRESET < 0
+        fprintf(masterScript, ' -precision %d', (TH_PRESET * -1));
+    else
+        fprintf(masterScript, ' -sensitivity %d', TH_PRESET);
+    end
+
+    fprintf(masterScript, ' -log \\"${coutDir}/${iname}_spots_mat.log\\"');
+    fprintf(masterScript, '" >> "${myChannelScript}"\n\n');
+    fprintf(masterScript, '\techo -e "else" >> "${myChannelScript}"\n');
+    fprintf(masterScript, '\techo -e "\\techo -e \\"Cellseg results not found! Terminating...\\"" >> "${myChannelScript}"\n');
+    fprintf(masterScript, '\techo -e "\\texit 1" >> "${myChannelScript}"\n');
+    fprintf(masterScript, '\techo -e "fi\\n" >> "${myChannelScript}"\n\n');
+
+    %Quant
+    fprintf(masterScript, '\tquantResFile="${coutDir}/${iname}_quantData.mat"\n');
+    fprintf(masterScript, '\techo -e "if [ -s \\"${spotsResStem}_callTable.mat\\" ]; then" >> "${myChannelScript}"\n');
+    fprintf(masterScript, '\techo -e "');
+    fprintf(masterScript, '\\t\\"${trueSpotDir}/TrueSpot_RNAQuant.sh\\"');
+    fprintf(masterScript, ' -runinfo \\"${spotsResStem}_rnaspotsrun.mat\\"');
+    fprintf(masterScript, ' -outdir \\"${coutDir}\\"');
+    fprintf(masterScript, ' -cellsegpath \\"${csResFile}\\"');
+    fprintf(masterScript, ' -coordtable \\"${spotsResStem}_callTable.mat\\"');
+    fprintf(masterScript, ' -workers %d', CPUS_PER_JOB);
+    fprintf(masterScript, ' -log \\"${coutDir}/${iname}_quant_mat.log\\"');
+    fprintf(masterScript, '" >> "${myChannelScript}"\n\n');
+    fprintf(masterScript, '\techo -e "else" >> "${myChannelScript}"\n');
+    fprintf(masterScript, '\techo -e "\\techo -e \\"Spot call results not found! Terminating...\\"" >> "${myChannelScript}"\n');
+    fprintf(masterScript, '\techo -e "\\texit 1" >> "${myChannelScript}"\n');
+    fprintf(masterScript, '\techo -e "fi\\n" >> "${myChannelScript}"\n\n');
+
+    %Image job
+    fprintf(masterScript, '\techo -e "if [ -s \\"${quantResFile}\\" ]; then" >> "${myCellsegScript}"\n');
+    fprintf(masterScript, '\techo -e "\\techo -e \\"Quant data already found! Skipping...\\"" >> "${myCellsegScript}"\n');
+    fprintf(masterScript, '\techo -e "else" >> "${myCellsegScript}"\n');
+    fprintf(masterScript, '\techo -e "\\tchmod 770 \\"${myChannelScript}\\"" >> "${myCellsegScript}"\n');
+    fprintf(masterScript, '\techo -e "\\tsbatch');
+    fprintf(masterScript, ' --job-name=\\"TS_${iname}\\"');
+    fprintf(masterScript, ' --cpus-per-task=%d', CPUS_PER_JOB);
+    fprintf(masterScript, ' --time=%d:00:00', HR_PER_JOB);
+    fprintf(masterScript, ' --mem=%dg', RAM_GB_PER_JOB);
+    fprintf(masterScript, ' --error=\\"${coutDir}/${iname}_truespot_slurm.err\\"');
+    fprintf(masterScript, ' --out=\\"${coutDir}/${iname}_truespot_slurm.out\\"');
+    fprintf(masterScript, ' \\"${myChannelScript}\\"');
+    fprintf(masterScript, '" >> "${myCellsegScript}"\n');
+    fprintf(masterScript, '\techo -e "fi\\n" >> "${myCellsegScript}"\n');
+
 end
 
-fprintf(masterScript, ' -log \\"${ioutDir}/${iname}_spots_mat.log\\"');
-fprintf(masterScript, '" >> "${myImgScript}"\n\n');
-fprintf(masterScript, '\techo -e "else" >> "${myImgScript}"\n');
-fprintf(masterScript, '\techo -e "\\techo -e \\"Cellseg results not found! Terminating...\\"" >> "${myImgScript}"\n');
-fprintf(masterScript, '\techo -e "\\texit 1" >> "${myImgScript}"\n');
-fprintf(masterScript, '\techo -e "fi\\n" >> "${myImgScript}"\n\n');
-
-%Quant
-fprintf(masterScript, '\tquantResFile="${ioutDir}/${iname}_quantData.mat"\n');
-fprintf(masterScript, '\techo -e "if [ -s \\"${spotsResStem}_callTable.mat\\" ]; then" >> "${myImgScript}"\n');
-fprintf(masterScript, '\techo -e "');
-fprintf(masterScript, '\\t\\"${trueSpotDir}/TrueSpot_RNAQuant.sh\\"');
-fprintf(masterScript, ' -runinfo \\"${spotsResStem}_rnaspotsrun.mat\\"');
-fprintf(masterScript, ' -outdir \\"${ioutDir}\\"');
-fprintf(masterScript, ' -cellsegpath \\"${csResFile}\\"');
-fprintf(masterScript, ' -coordtable \\"${spotsResStem}_callTable.mat\\"');
-fprintf(masterScript, ' -workers %d', CPUS_PER_JOB);
-fprintf(masterScript, ' -log \\"${ioutDir}/${iname}_quant_mat.log\\"');
-fprintf(masterScript, '" >> "${myImgScript}"\n\n');
-fprintf(masterScript, '\techo -e "else" >> "${myImgScript}"\n');
-fprintf(masterScript, '\techo -e "\\techo -e \\"Spot call results not found! Terminating...\\"" >> "${myImgScript}"\n');
-fprintf(masterScript, '\techo -e "\\texit 1" >> "${myImgScript}"\n');
-fprintf(masterScript, '\techo -e "fi\\n" >> "${myImgScript}"\n\n');
-
 %Image job
-fprintf(masterScript, '\techo -e "if [ -s \\"${ipath}\\" ]; then" >> "${submitScript}"\n');
-fprintf(masterScript, '\techo -e "\\tif [ -s \\"${quantResFile}\\" ]; then" >> "${submitScript}"\n');
-fprintf(masterScript, '\techo -e "\\t\\techo -e \\"Quant data already found! Skipping...\\"" >> "${submitScript}"\n');
-fprintf(masterScript, '\techo -e "\\telse" >> "${submitScript}"\n');
-fprintf(masterScript, '\techo -e "\\t\\tchmod 770 \\"${myImgScript}\\"" >> "${submitScript}"\n');
-fprintf(masterScript, '\techo -e "\\t\\tsbatch');
-fprintf(masterScript, ' --job-name=\\"TS_${iname}\\"');
-fprintf(masterScript, ' --cpus-per-task=%d', CPUS_PER_JOB);
-fprintf(masterScript, ' --time=%d:00:00', HR_PER_JOB);
-fprintf(masterScript, ' --mem=%dg', RAM_GB_PER_JOB);
-fprintf(masterScript, ' --error=\\"${ioutDir}/${iname}_truespot_slurm.err\\"');
-fprintf(masterScript, ' --out=\\"${ioutDir}/${iname}_truespot_slurm.out\\"');
-fprintf(masterScript, ' \\"${myImgScript}\\"');
+fprintf(masterScript, '\n\techo -e "if [ -s \\"${ipath}\\" ]; then" >> "${submitScript}"\n');
+%fprintf(masterScript, '\techo -e "\\tif [ -s \\"${quantResFile}\\" ]; then" >> "${submitScript}"\n');
+%fprintf(masterScript, '\techo -e "\\t\\techo -e \\"Quant data already found! Skipping...\\"" >> "${submitScript}"\n');
+%fprintf(masterScript, '\techo -e "\\telse" >> "${submitScript}"\n');
+fprintf(masterScript, '\techo -e "\\tchmod 770 \\"${myCellsegScript}\\"" >> "${submitScript}"\n');
+fprintf(masterScript, '\techo -e "\\tsbatch');
+fprintf(masterScript, ' --job-name=\\"TSCS_${fname}\\"');
+fprintf(masterScript, ' --cpus-per-task=%d', CPUS_PER_JOB_CELLSEG);
+fprintf(masterScript, ' --time=%d:00:00', HR_PER_JOB_CELLSEG);
+fprintf(masterScript, ' --mem=%dg', RAM_GB_PER_JOB_CELLSEG);
+fprintf(masterScript, ' --error=\\"${ioutDir}/${fname}_ts_cellseg_slurm.err\\"');
+fprintf(masterScript, ' --out=\\"${ioutDir}/${fname}_ts_cellseg_slurm.out\\"');
+fprintf(masterScript, ' \\"${myCellsegScript}\\"');
 fprintf(masterScript, '" >> "${submitScript}"\n');
-fprintf(masterScript, '\techo -e "\\tfi" >> "${submitScript}"\n');
+%fprintf(masterScript, '\techo -e "\\tfi" >> "${submitScript}"\n');
 fprintf(masterScript, '\techo -e "fi\\n" >> "${submitScript}"\n\n');
 
 fprintf(masterScript, 'done < ${imageListPath}\n\n');
